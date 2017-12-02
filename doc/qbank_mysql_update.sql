b64e95ba9ea7380114415ca9af66d223f3f917ff
ChuckZHENG <zhengcongchun@gmail.com>
2017-12-03 00:16:59 +0800

    init develop branch

--- doc/qbank_mysql_update.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE exercise
-  ADD COLUMN
-  chapter              varchar(50) comment '所属课本章节';
-
-ALTER TABLE exercise
-  ADD COLUMN cert_level           int comment '所属证书等级：1-一级, 2-二级， 3-三级';
